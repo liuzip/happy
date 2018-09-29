0d9e073840073dd0cd67d7a33fd92c3dd2aae5c3
liuzihao <liuzih@133.cn>
2018-09-29 15:47:46 +0800

    用户首页/登陆逻辑

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import ui from './components/ui/index'
 import Index from './index'
 import router from './router'
+import store from './store'
 
 Vue.prototype.$withLoading = ui.withLoading
 Vue.prototype.$popup = ui.showPopup
@@ -13,6 +14,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   router,
+  store,
   components: { Index },
   template: '<Index/>'
 }).$mount('#app')
--- src/main/index.vue
+++ src/main/index.vue
@@ -4,31 +4,41 @@
 </template>
 
 <script>
-import localStorage from '@/utils/localStorage'
+import { mapState, mapGetters, mapMutations } from 'vuex'
+
 export default {
-  data () {
-    return {
-      signed: false,
-      config: {}
-    }
+  computed: {
+    ...mapState([
+      'signed',
+      'config'
+    ]),
+    ...mapGetters([
+      'unsingedWithAlias',
+      'isNewUser'
+    ])
   },
   created () {
-    this.config = localStorage.get('config')
-    if (!this.signed) {
-      if (this.config && this.config.alias) {
-        this.$popup({
-          content: `确认使用账号${this.config.alias}（${this.config.phone}）登陆吗？`,
-          showCancelButton: true,
-          confirm: () => {
-            console.log('click confirm')
-          }
-        })
-      } else {
-        this.$router.push({
-          name: 'login'
-        })
-      }
+    this.initStore()
+    if (this.isNewUser) {
+      this.$router.push({
+        name: 'login'
+      })
+    } else if (this.unsingedWithAlias) {
+      this.$popup({
+        content: `确认使用账号${this.config.alias}（${this.config.phone}）登陆吗？`,
+        showCancelButton: true,
+        confirmButtonText: '换个账号',
+        cancelButtonText: '确认',
+        confirm: () => {
+          this.$router.push({
+            name: 'login'
+          })
+        }
+      })
     }
+  },
+  methods: {
+    ...mapMutations(['initStore'])
   }
 }
 </script>
